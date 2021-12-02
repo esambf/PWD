52169673c8fc422e973aaea61175cd71f49f9cb8
esambf <embfalahiedha@gmail.com>
2021-12-02 22:57:50 +0700

    prak 7

---
+++ prak7/logout.php
@@ -0,0 +1,5 @@
+<?php
+session_start(); #memulai session
+session_destroy(); #menghapus dan menghentikan session
+echo "Anda telah sukses keluar sistem <b>LOGOUT</b>"; #keterangan logout berhasil
+?>
